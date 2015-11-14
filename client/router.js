ffee074c7639497e56bd4cabdd16686f03339463
mrbabbs <gioesps@gmail.com>
2015-11-14 14:58:06 +0100

    Added route to home page

--- client/router.js
+++ client/router.js
@@ -1,11 +1,7 @@
 var app = require('ampersand-app');
 var Router = require('ampersand-router');
-var HomePage = require('./pages/home');
-var CollectionDemo = require('./pages/collection-demo');
-var InfoPage = require('./pages/info');
-var PersonAddPage = require('./pages/person-add');
-var PersonEditPage = require('./pages/person-edit');
-var PersonShowPage = require('./pages/person-show');
+var NewGamePage = require('./pages/new-game');
+
 
 
 module.exports = Router.extend({
@@ -21,38 +17,7 @@ module.exports = Router.extend({
 
     // ------- ROUTE HANDLERS ---------
     home: function () {
-        app.trigger('page', new HomePage({
-            model: app.me
-        }));
-    },
-
-    collectionDemo: function () {
-        app.trigger('page', new CollectionDemo({
-            model: app.me,
-            collection: app.people
-        }));
-    },
-
-    info: function () {
-        app.trigger('page', new InfoPage({
-            model: app.me
-        }));
-    },
-
-    personAdd: function () {
-        app.trigger('page', new PersonAddPage());
-    },
-
-    personEdit: function (id) {
-        app.trigger('page', new PersonEditPage({
-            id: id
-        }));
-    },
-
-    personView: function (id) {
-        app.trigger('page', new PersonShowPage({
-            id: id
-        }));
+        app.trigger('page', new NewGamePage());
     },
 
     catchAll: function () {
---
+++ test/specs/spec.router.js
@@ -0,0 +1,17 @@
+var Router = require('../../client/router');
+
+describe('Router', function () {
+    beforeEach(function () {
+        this.router = new Router();
+    });
+
+    it('should be defined', function () {
+        expect(this.router).toBeDefined();
+    });
+
+    describe('when is initialized', function () {
+        it('should have a function to go home page', function () {
+            expect(this.router.home).toBeDefined();
+        });    
+    });
+});
